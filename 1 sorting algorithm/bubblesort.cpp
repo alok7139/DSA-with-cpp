025601cf90a4d156a094faf0da3bfbdb5baa4caa
alok7139 <alokgarg7139@gmail.com>
2024-06-11 15:51:01 +0530

    Update bubblesort.cpp

--- 1 sorting algorithm/bubblesort.cpp
+++ 1 sorting algorithm/bubblesort.cpp
@@ -1,15 +1,19 @@
 /*
-statement
 Given an Integer N and a list arr. Sort the array using bubble sort algorithm.
-example
+Example 1:
+
 Input: 
 N = 5
 arr[] = {4, 1, 3, 9, 7}
 Output: 
 1 3 4 7 9
-*\
+*/
 
- void bubbleSort(int arr[], int n)
+class Solution
+{
+    public:
+    //Function to sort the array using bubble sort algorithm.
+    void bubbleSort(int arr[], int n)
     {
         // Your code here
       for(int i=n-1;i>=0;i--){
@@ -20,3 +24,4 @@ Output:
         }
     }  
     }
+};
