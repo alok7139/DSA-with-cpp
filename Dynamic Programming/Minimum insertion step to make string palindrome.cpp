c1574449f1a63a46e1abe918c63aba5dffc82ba5
alok7139 <alokgarg7139@gmail.com>
2024-07-10 21:56:38 +0530

    Create Minimum insertion step to make string palindrome.cpp

---
+++ Dynamic Programming/Minimum insertion step to make string palindrome.cpp
@@ -0,0 +1,53 @@
+/*
+Given a string s. In one step you can insert any character at any index of the string.
+
+Return the minimum number of steps to make s palindrome.
+
+A Palindrome String is one that reads the same backward as well as forward.
+
+ 
+
+Example 1:
+
+Input: s = "zzazz"
+Output: 0
+Explanation: The string "zzazz" is already palindrome we do not need any insertions.
+Example 2:
+
+Input: s = "mbadm"
+Output: 2
+Explanation: String can be "mbdadbm" or "mdbabdm".
+Example 3:
+
+Input: s = "leetcode"
+Output: 5
+Explanation: Inserting 5 characters the string becomes "leetcodocteel".
+*/
+
+class Solution {
+public:
+    int minInsertions(string s) {
+        string t=s;
+	reverse(s.begin(),s.end());
+	int len= lcs(t,s);
+	return s.size()-len;
+        
+    }
+int lcs(string s,string t){
+	int n=s.size();
+	int m=t.size();
+	vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+	
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=m;j++){
+			if(s[i-1]==t[j-1]){
+				dp[i][j]=1+dp[i-1][j-1];
+			}
+			else{
+				dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+			}
+		}
+	}
+	return dp[n][m];
+}
+};
